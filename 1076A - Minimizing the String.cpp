913cc70f17a10d563831a009620ebf7e0657eb6a
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2018-11-22 03:23:07 +0600

    Add files via upload

---
+++ 1076A - Minimizing the String.cpp
@@ -0,0 +1,33 @@
+/// Author: Mosiur Rahman Sweet
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    string s;
+    cin >> s;
+
+    int pos=num-1;
+
+    for(int i=1; s[i]; i++)
+    {
+        if(s[i-1]>s[i])
+        {
+            pos = i-1;
+            break;
+        }
+
+    }
+
+    for(int i=0; s[i]; i++)
+    {
+        if(pos != i)
+            cout << s[i];
+    }
+    cout << endl;
+
+    return 0;
+}
